36b4fe02ce2ab5baf2c365283d54276377184793
Orphee <orphee.lonie@gmail.com>
2023-08-26 16:18:16 +0200

    [main] try implemmenting non blocking input

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    /*
+    struct termios oldt, newt;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    */
+
+    /*
+    struct termios term;
+    cfmakeraw(&term);
+    */
+
+    while(1)
+    {
+        if(kbhit())
+        {
+            char c = getchar();
+            if (c == 'a')
+                printf("This is a 'a'\n");
+            else
+                printf("This is not a 'a'\n");
+        }
+    }
 
     return 0;
 }
