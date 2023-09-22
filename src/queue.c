c745f9567d29726055de0a12ee94a939924cc4ca
Orphee <orphee.lonie@gmail.com>
2023-09-22 20:36:40 +0200

    [queue] pass test_create_queue

--- src/queue.c
+++ src/queue.c
@@ -4,7 +4,23 @@
 
 Queue *new_queue(unsigned int max_size)
 {
-    return malloc(sizeof(struct Queue));
+    Queue *q = malloc(sizeof(struct Queue));
+    if (q == NULL)
+        return NULL;
+
+    q->array = malloc(sizeof(int) * max_size);
+    if (q->array == NULL)
+    {
+        free(q);
+        return NULL;
+    }
+
+    q->max_size = max_size;
+    q->size = 0;
+    q->head = 0;
+    q->tail = 0;
+
+    return q;
 }
 
 void delete_queue(Queue *q)
--- test/test_main.c
+++ test/test_main.c
@@ -6,13 +6,18 @@
 void test_create_queue()
 {
     // Given
-    int output = 1;
+    int max_size = 10;
 
     // When
-    Queue *q = new_queue(10);
+    Queue *q = new_queue(max_size);
 
     // Then
     assert(q != NULL);
+    assert(q->array != NULL);
+    assert(q->max_size == max_size);
+    assert(q->size == 0);
+    assert(q->head == 0);
+    assert(q->tail == 0);
 
     delete_queue(q);
 }
