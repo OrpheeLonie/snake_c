268b595c806bb88234bd4684a0378ceb381ad1e8
Orphee <orphee.lonie@gmail.com>
2023-09-24 21:34:17 +0200

    [utils] add clear_screen()

--- src/utils.c
+++ src/utils.c
@@ -4,3 +4,8 @@ void gotoXY(int x, int y)
 {
     printf("\e[%d;%dH", y, x);
 }
+
+void clear_screen()
+{
+    printf("\e[2J");
+}
--- src/utils.h
+++ src/utils.h
@@ -2,5 +2,6 @@
 #define UTILS_H
 
 void gotoXY(int x, int y);
+void clear_screen();
 
 #endif // UTILS_H
