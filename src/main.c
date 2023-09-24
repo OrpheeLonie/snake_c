528331722b5dd5f330752717f35484f23ba36097
Orphee <orphee.lonie@gmail.com>
2023-09-24 22:07:22 +0200

    [main] add fcntl

--- src/main.c
+++ src/main.c
@@ -13,8 +13,12 @@ int main()
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    // tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    int oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    // fcntl(STDIN_FILENO, O_NONBLOCK);
 
     // system("clear");
     printf("\e[2J");
@@ -23,6 +27,12 @@ int main()
         gotoXY(20+i, 10);
         printf("#\n");
         usleep(200000);
+        char c = getc(stdin);
+        if (c != EOF)
+        {
+            gotoXY(i, 0);
+            printf("%c\n", c);
+        }
     }
 
     return 0;
